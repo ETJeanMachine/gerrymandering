5f4e32f43ea066ed0f341759be1d554d0f4b1424
ETJeanMachine <ericjean873@gmail.com>
2021-10-19 01:24:33 -0400

    Basic main function (compiles and runs)

--- src/Gerrymandering.cpp
+++ src/Gerrymandering.cpp
@@ -1,4 +1,9 @@
-int main(int argc, char const *argv[])
-{
+#include "Node.hpp"
+#include <iostream>
+
+int main(int argc, char const* argv[]) {
+    int n = 0;
+    Node node(&n);
+    std::cout << node.str() << std::endl;
     return 0;
 }
