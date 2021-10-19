a7c02af28730055d65638aeb929d4fe301ffb234
ETJeanMachine <ericjean873@gmail.com>
2021-10-19 01:22:44 -0400

    Started work on Node.cpp

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,3 +1,29 @@
-class Node {
-    
-};
+#include "Node.hpp"
+#include <string>
+
+Node::Node(void* d) {
+    data = d;
+    neighboursSize = 0;
+    neighbours; // TODO
+}
+
+void* Node::getData() {
+    return data;
+}
+
+void Node::addNeighbour(Node* n) {
+    // TODO
+}
+
+Node** Node::getNeighbours() {
+    return neighbours;
+}
+
+std::string Node::toString() {
+    // TODO
+    std::string out = "";
+    for (int i = 0; i < neighboursSize; i++) {
+
+    }
+    return out;
+}
