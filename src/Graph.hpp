68b1cb35b163c6895b44bd04d3983764953a6f9f
ETJeanMachine <ericjean873@gmail.com>
2021-10-20 12:01:12 -0400

    Working on connecting nodes

--- src/Graph.hpp
+++ src/Graph.hpp
@@ -1,6 +1,21 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
 
+#include <unordered_map>
+#include "Node.hpp"
 
+template<class T = void>
+class Graph {
+private:
+    std::unordered_map<T, Node<T>*> map;
+public:
+    void createNode(T data) {
+        map[data] = new Node<T>(data);
+    }
+    bool connectNodes(T data1, T data2) {
+        if (map.find(data1) && map.find(data2))
+            
+    }
+};
 
 #endif
