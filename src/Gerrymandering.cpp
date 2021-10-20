22ff3b829b1c3ce47fe70a87c7de9ddeebe456ea
ETJeanMachine <ericjean873@gmail.com>
2021-10-20 11:24:55 -0400

    Some testing

--- src/Gerrymandering.cpp
+++ src/Gerrymandering.cpp
@@ -4,7 +4,12 @@
 
 int main(int argc, char const* argv[]) {
     int a = 1;
-    Node node(&a);
-    std::cout << node << std::endl;
+    Node<int> node(&a);
+    for (int i = 2; i < 6; i++) {
+        int* x = new int;
+        *x = i;
+        node.addNeighbour(new Node<int>(x));
+    }
+    std::cout << node.str() << std::endl;
     return 0;
 }
